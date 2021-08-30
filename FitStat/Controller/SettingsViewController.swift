b99422506c0324c2955348d1567c93291e95461a
Lynda Chiwetelu <lyndachiwe@gmail.com>
2021-08-30 18:31:32 +0200

    fix picker views in reminder settings

--- FitStat/Controller/SettingsViewController.swift
+++ FitStat/Controller/SettingsViewController.swift
@@ -8,11 +8,27 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-
+    @IBOutlet var foodLogIntervalPicker: UIPickerView!
+    @IBOutlet var moodLogIntervalPicker: UIPickerView!
+    @IBOutlet var bodyMetricLogIntervalPicker: UIPickerView!
+    @IBOutlet var workoutLogIntervalPicker: UIPickerView!
+    @IBOutlet var weightLogIntervalPicker: UIPickerView!
+    @IBOutlet var sleepLogIntervalPicker: UIPickerView!
+    
+    var times = ["Everyday", "Every Monday", "Every Tuesday", "Every Wednesday", "Every Thursday", "Every Friday", "Every Saturday", "Every Sunday" ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setDelegates()
+    }
+    
+    func setDelegates() {
+        let views = [foodLogIntervalPicker, moodLogIntervalPicker, bodyMetricLogIntervalPicker, workoutLogIntervalPicker, weightLogIntervalPicker, sleepLogIntervalPicker]
+        for view in views {
+            view?.delegate = self
+            view?.dataSource = self
+        }
+        
     }
     
     @IBAction func saveRemindersPressed(_ sender: UIButton) {
@@ -20,15 +36,22 @@ class SettingsViewController: UIViewController {
         let mainTabBarController = storyboard.instantiateViewController(identifier: "MainTabBarController")
         (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.changeRootViewController(mainTabBarController)
     }
-    
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+}
+
+extension SettingsViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
-    */
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return times.count
+    }
+}
 
+extension SettingsViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return times[row]
+    }
+    
 }
