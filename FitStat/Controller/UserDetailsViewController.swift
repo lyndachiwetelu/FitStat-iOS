ac04fce930bce0aee9ff488f384d290af18e5f47
Lynda Chiwetelu <lyndachiwe@gmail.com>
2021-10-01 22:39:43 +0200

    uipicker datasource and delegates

--- FitStat/Controller/UserDetailsViewController.swift
+++ FitStat/Controller/UserDetailsViewController.swift
@@ -15,12 +15,56 @@ class UserDetailsViewController: UIViewController {
     @IBOutlet var weightUnitPicker: UIPickerView!
     @IBOutlet var heightUnitPicker: UIPickerView!
     
+    var weightUnits = [
+        "Kilograms",
+        "Pounds"
+    ]
+    
+    var heightUnits = [
+        "metres",
+        "centimeters",
+    ]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        weightUnitPicker.dataSource = self
+        weightUnitPicker.delegate = self
+        heightUnitPicker.dataSource = self
+        heightUnitPicker.delegate = self
     }
 
     @IBAction func continueButtonPressed(_ sender: UIButton) {
         performSegue(withIdentifier: AppConstant.segueToMainView, sender: self)
     }
 }
+
+extension UserDetailsViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        switch pickerView.tag {
+        case 100:
+            return weightUnits.count
+        default:
+            return heightUnits.count
+        }
+    }
+    
+    
+}
+
+extension UserDetailsViewController: UIPickerViewDelegate{
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        switch pickerView.tag {
+        case 100:
+            return weightUnits[row]
+        default:
+            return heightUnits[row]
+        }
+        
+    }
+}
+
